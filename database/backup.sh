976c31bfa2fe10e042f8be4e961c7ccb04099873
jrgavilanes@gmail.com <jrgavilanes@gmail.com>
2024-06-15 12:55:48 +0200

    añade backup bd base y restaura color fondo login

--- database/backup.sh
+++ database/backup.sh
@@ -1,3 +1,3 @@
 echo "Backup de la base de datos..."
-docker exec postgis-eiel pg_dump -U janrax -d sig_local | gzip > db_backup_$(date +%Y%m%d%H%M%S).sql.gz
+docker exec tfg-postgis-eiel pg_dump -U janrax -d sig_local | gzip > db_backup_$(date +%Y%m%d%H%M%S).sql.gz
 echo "Backup y compresión de la base de datos finalizado."
